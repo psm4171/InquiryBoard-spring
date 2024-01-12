c5632ea7b470dbd8034ffbb9ce1d12ad0ea25e76
{psm4171} <{kkjjhh4171@naver.com}>
2024-01-12 19:39:06 +0900

    feat: Post

--- src/main/java/com/nhnacademy/springmvc/domain/Post.java
+++ src/main/java/com/nhnacademy/springmvc/domain/Post.java
@@ -1,4 +1,27 @@
 package com.nhnacademy.springmvc.domain;
 
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@NoArgsConstructor
+@Getter
+@Setter
 public class Post {
+
+    long postId;
+
+    String writeTime;
+    String writerId;
+    String category;
+    String title;
+    String content;
+    Post reply;
+
+    public Post(long postId, String category, String title, String content) {
+        this.postId = postId;
+        this.category = category;
+        this.title = title;
+        this.content = content;
+    }
 }
