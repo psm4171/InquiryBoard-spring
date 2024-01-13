cef1bb3266777be3a53f6c6c0cab6022a6a70a20
{psm4171} <{kkjjhh4171@naver.com}>
2024-01-14 03:42:12 +0900

    modify: PostRepository & PostRepositoryImpl

--- src/main/java/com/nhnacademy/springmvc/domain/Post.java
+++ src/main/java/com/nhnacademy/springmvc/domain/Post.java
@@ -1,27 +1,37 @@
 package com.nhnacademy.springmvc.domain;
 
+import java.time.LocalDateTime;
+import java.util.List;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import lombok.ToString;
 
-@NoArgsConstructor
 @Getter
 @Setter
+@ToString
 public class Post {
 
-    long postId;
+    private long postId;
 
-    String writeTime;
-    String writerId;
-    String category;
-    String title;
-    String content;
-    Post reply;
+    private String accountId;
+    private String title;
+    private Category category;
+    private String content;
+    private LocalDateTime registerDate;
 
-    public Post(long postId, String category, String title, String content) {
-        this.postId = postId;
-        this.category = category;
+    private boolean answerStatus;
+
+    public Post(String accountId, String title, Category category, String content) {
+        this.accountId = accountId;
         this.title = title;
+        this.category = category;
         this.content = content;
     }
+
+    public static Post create(String accountId, String title, Category category, String content){
+        return new Post(accountId, title, category, content);
+    }
+
+
 }
